4b9abe5f65c0ac0b851e8975dcbafdfb1b095441
Tomás Fernandes <narotuninja21@gmail.com>
2026-03-24 11:32:05 +0000

    mensagem descritiva

--- cabazes/views.py
+++ cabazes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Cabaz
 
-# Create your views here.
+def lista_cabazes(request):
+    cabazes = Cabaz.objects.all()
+    return render(request, 'cabazes/lista.html', {'cabazes': cabazes})
